698f46382e01c3f6d9f02518a4fe39d3d80539c3
kentaro <kentaro.arai@bestand.jp>
2022-04-09 23:34:18 +0900

    GTM tag inserted

--- header.php
+++ header.php
@@ -12,6 +12,9 @@
 <html <?php language_attributes(); ?>>
 
 <head>
+	<!-- Google Tag Manager -->
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-KTWRLTF');</script>
+	<!-- End Google Tag Manager -->
 	<title>株式会社BESTAND（ビースタンド）</title>
 	<meta name="description" content="株式会社BESTANDは、顧客企業の成功・成長を幅広く支援します。"/>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
@@ -19,19 +22,22 @@
 	<script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
 
 	<!-- Global site tag (gtag.js) - Google Analytics -->
-	<script async src="https://www.googletagmanager.com/gtag/js?id=G-QX0NBEBS6Q"></script>
+	<!-- <script async src="https://www.googletagmanager.com/gtag/js?id=G-QX0NBEBS6Q"></script>
 	<script>
   		window.dataLayer = window.dataLayer || [];
   		function gtag(){dataLayer.push(arguments);}
   		gtag('js', new Date());
   		gtag('config', 'G-QX0NBEBS6Q');
-	</script>
+	</script> -->
 
 	<?php wp_head(); ?>
 
 </head>
 
 <body <?php body_class(); ?>>
+	<!-- Google Tag Manager (noscript) -->
+	<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KTWRLTF" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+	<!-- End Google Tag Manager (noscript) -->
 	<?php wp_body_open(); ?>
 	
 	<div>
